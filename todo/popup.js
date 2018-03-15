bc25739415bf33ecdcbb96e64a62c16c39427f9f
akhil_001 <akhilsairammaruti@gmail.com>
2018-03-15 10:21:20 +0530

    Just trying to optimize app

--- todo/popup.js
+++ todo/popup.js
@@ -1,12 +1,13 @@
 function addToList()
 {
     var unlistedItems=document.getElementById("items");
-    var li=document.createElement("li");
-    var newItem=document.getElementById("item-value").innerText;
-    li.appendChild(document.createTextNode(newItem));
-    unlistedItems.appendChild(li);
+    // var li=document.createElement("li");
+     var newItem=document.getElementById("item-value").value;
+    // li.appendChild(document.createTextNode(newItem));
+    // unlistedItems.appendChild(li);
+    unlistedItems.innerHTML+="<li>"+newItem+"</li>";
 }
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button').addEventListener('click', addToList);
-    main();
+    //main();
   });
