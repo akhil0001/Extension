98957a1a1979748bb1bbd5c44fac44ff453798fe
akhil_001 <akhilsairammaruti@gmail.com>
2018-04-10 10:11:59 +0530

    FInal Commit

--- todo/sidebar.js
+++ todo/sidebar.js
@@ -10,15 +10,27 @@ var tempListItem;
 
 
 saveTheButton.addEventListener('click',function(){
+  var titleOfTask = document.getElementById('task_title').value;
+  var Description = document.getElementById('task_description').value;
+  var colorSelected = '#42A5F5';
+  //hidetheContainer and load the diiv with the specific animation
+  storeTask(titleOfTask,Description);
+  hideThetodoGridContainer();
+
+})
+
+function hideThetodoGridContainer() {
   todoGridContainer.style.visibility = 'visible';
   addTaskContainer.style.visibility='hidden';
-})
+}
 
 addButton.addEventListener('click',function(){
   tempListItem = '<li>Hello There I am freaking awesome</li>';
-  var title = 'Lol';
-  var body = 'Hello there! This is akhil! I am a wannabe superhero. ok '
-  // storeTask(title,body)
+  var titleOfTask = document.getElementById('task_title');
+  var Description = document.getElementById('task_description');
+  var colorSelected = '#42A5F5';
+  titleOfTask.value = "";
+  Description.value = "";
   todoGridContainer.style.visibility = 'hidden';
   addTaskContainer.style.visibility='visible';
 })
@@ -55,13 +67,15 @@ var titleItem = document.createElement('h3');
 var doneButton = document.createElement('button');
 var taskBody = document.createElement('p');
 var priorityTask = document.createElement('div')
+var editButton = document.createElement('button');
 
 titleItem.textContent = title;
 taskBody.textContent = body;
 doneButton.innerHTML = '<i class="material-icons md-18">delete</i>';
+editButton.innerHTML = '<i class="material-icons md-18">mode_edit</i>'
 
 doneButton.addEventListener('click',function(e){
-  console.dir(e);
+  browser.storage.local.remove(e.target.parentNode.childNodes[1].innerText);
   e.target.parentNode.parentNode.removeChild(e.target.parentNode);
   console.dir(listitemsOfTodoTasks.childNodes);
   if (listitemsOfTodoTasks.childNodes.length==1) {
@@ -69,14 +83,19 @@ doneButton.addEventListener('click',function(e){
     p.textContent = 'No Tasks'
     listitemsOfTodoTasks.appendChild(p);
   }
+  
 //  shiftTheTaskfromTodotoDoneList(title,e,body);
 })
+
+editButton.addEventListener('click',function(){
+  alert('Hi Working');
+});
 
 listItem.appendChild(priorityTask);
 listItem.appendChild(titleItem);
 listItem.appendChild(taskBody);
 listItem.appendChild(doneButton);
-
+// listItem.appendChild(editButton);
 
 
 listitemsOfTodoTasks.appendChild(listItem);
